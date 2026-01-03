c30a10a0426dd47acb669924591a44bb3bcee454
jatinparmar68301 <jatinparmar68301@gmail.com>
2026-01-03 15:09:11 +0530

    Add files via upload

---
+++ 1_task.py
@@ -0,0 +1,25 @@
+# ask
+# write a program to create variables to store and display below person detail
+# name
+# address
+# city
+# pincode
+# age
+# weight
+# gender
+
+name = 'Parmar jatin'
+address = '10, main street'
+city = 'Bhavnagar'
+pincode = '364004'
+age =20
+weight = 40
+gender = 'male'
+print("Name:", name)
+print("Address:", address)  
+print("City:", city)
+print("Pincode:", pincode)
+print("Age:", age)
+print("Weight:", weight)
+print("Gender:", gender)
+# endask
