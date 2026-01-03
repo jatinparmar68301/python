d6feee95974c087cd80960d851274f693f816135
jatinparmar68301 <jatinparmar68301@gmail.com>
2026-01-03 15:10:10 +0530

    Add files via upload

---
+++ excercize/1_ex.py
@@ -0,0 +1,14 @@
+# write program to calculate and display are of rectangle using given length and width
+# variable create
+
+length=0
+width=0
+
+length=input("Enter length=")
+width=input("Enter width=")
+
+length=int(length)
+width=int(width)
+
+area=length*width
+print("Area is=",area)
