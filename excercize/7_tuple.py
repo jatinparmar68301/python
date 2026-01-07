d9f9527604a19d232f104387e1fded0dc4dfe8f3
jatinparmar68301 <jatinparmar68301@gmail.com>
2026-01-07 15:29:50 +0530

    Add files via upload

---
+++ excercize/7_tuple.py
@@ -0,0 +1,23 @@
+# create tuple to store all states name of india 
+
+states= ("Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal")
+
+# display tuple
+
+print(states)
+
+#display 1st five items
+
+print(states[0:5])
+
+#display 2nd and 3rd and 4th item
+
+print(states[2:5])
+
+#display all item from 7th positin on words
+
+print(states[6:])
+
+#try to remove 3rd item see what happen
+
+# states.remove("Assam") are show the error
