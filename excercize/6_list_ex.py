0295cfa659a0b6e1cb39b7f3fb4c5b861c21cdd0
jatinparmar68301 <jatinparmar68301@gmail.com>
2026-01-07 15:29:23 +0530

    Add files via upload

---
+++ excercize/6_list_ex.py
@@ -0,0 +1,62 @@
+#create a list to store your favourite destination in india
+# create a list & store 10 different type of value in it
+
+list1=['Goa','Manali','Jaipur','Rajasthan','MountAbu','HimachalPradesh','Kerala']
+
+list2=['Jay Mataji',47,88.56,False,'Mahadev',356,None,'India',(4,1),('Jatin Parmar')]
+
+#use of list1
+
+#print all values
+
+print(list1)
+
+#print 1st  value
+
+print(list1[0])
+
+#print first five value
+
+print(list1[0:6])
+
+#print last 5 values
+
+print(list1[2:])
+
+#print list twice using *
+
+print(list1 *2)
+
+#print item in the list from 2nd to 5th position
+
+print(list1[2:6])
+
+#----------------------------------------------------------------------------------------------
+# use of list2
+
+#print all values
+
+print(list2)
+
+#print 1st  value
+
+print(list2[0])
+
+#print first five value
+
+print(list2[0:6])
+
+#print last 5 values
+
+print(list2[5:])
+
+#print list twice using *
+
+print(list2 *2)
+
+#print item in the list from 2nd to 5th position
+
+print(list2[2:6])
+
+
+
