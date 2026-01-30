10da0c1bd6e754ea73b95e8aad7fe1d0c7a33f90
jatinparmar68301 <jatinparmar68301@gmail.com>
2026-01-30 15:43:19 +0530

    Jatin Parmar

---
+++ excercize/28_while_2.py
@@ -0,0 +1,8 @@
+#  1    8   27   125  ....... 1000
+
+num = 1
+print(num,end=' ') #1
+
+while num<=9:
+    num = num + 1 #2
+    print(num*num*num,end=' ')
---
+++ excercize/29_while_3.py
@@ -0,0 +1,10 @@
+# 1    -8   27  -64  .....    1000
+
+num1=1
+num2=1
+
+while num1<=10:
+    print(num2*(num1*num1*num1),end=' ')
+    num2=-num2
+    num1=num1+1
+
